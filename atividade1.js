e08b9d4e9a0713542d2445452963c884d19e06d1
Amanda <amandamsilvasantos96@gmail.com>
2021-08-30 19:23:04 -0300

    exercicios da semana 2

---
+++ atividade1.js
@@ -0,0 +1,20 @@
+var letra = prompt("Digite uma letra: ");
+
+if(letra == "a"){
+    alert("Você digitou uma vogal!")
+} 
+else if(letra == "e"){
+    alert("Você digitou uma vogal!")
+} 
+else if(letra == "i"){
+    alert("Você digitou uma vogal!")
+} 
+else if(letra == "o"){
+    alert("Você digitou uma vogal!")
+} 
+else if(letra == "u"){
+    alert("Você digitou uma vogal!")
+} 
+else { 
+    alert("Você digitou uma consoante")
+}
---
+++ exercicio2.js
@@ -0,0 +1,4 @@
+var num1 = Number(prompt("Insira um número: "));
+var num2 = Number(prompt("Insira um número: "));
+
+alert(num1 + num2)
